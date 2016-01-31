e30ca7ffa7e12f305459562174aa6a8303648f1a
him11235 <ben.jarnagin@gmail.com>
2016-01-31 18:05:50 -0500

    mod0 t2 rev2

--- labs/0/2-Preliminary/T2/settings/in_out.Debug.cspy.bat
+++ labs/0/2-Preliminary/T2/settings/in_out.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "G:\labs\0\2-Preliminary\T2\settings\in_out.Debug.general.xcl" --backend -f "G:\labs\0\2-Preliminary\T2\settings\in_out.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "G:\labs\0\2-PRELIMINARY\T2\settings\in_out.Debug.general.xcl" --backend -f "G:\labs\0\2-PRELIMINARY\T2\settings\in_out.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "G:\labs\0\2-Preliminary\T2\settings\in_out.Debug.general.xcl" "--debug_file=%~1" --backend -f "G:\labs\0\2-Preliminary\T2\settings\in_out.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "G:\labs\0\2-PRELIMINARY\T2\settings\in_out.Debug.general.xcl" "--debug_file=%~1" --backend -f "G:\labs\0\2-PRELIMINARY\T2\settings\in_out.Debug.driver.xcl" 
 
 @echo off 
 :end
